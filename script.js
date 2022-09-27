04d386500d411b3b4a82d94d5882f9d171f14bfb
barryadrianallen <barryadrianallen@gmail.com>
2022-09-27 21:13:47 +0100

    add javascript calculations

--- script.js
+++ script.js
@@ -2,14 +2,48 @@ let inputtedSphere = document.getElementById("sphere-input");
 let inputtedCylinder = document.getElementById("cylinder-input");
 let inputtedAxis = document.getElementById("axis-input");
 let convertButton = document.getElementById("convert");
-let convertedSphere = document.getElementById("converted-sphere");
-let convertedCylinder = document.getElementById("converted-cylinder");
-let convertedAxis = document.getElementById("converted-axis");
+let convertedSphereOutput = document.getElementById("converted-sphere");
+let convertedCylinderOutput = document.getElementById("converted-cylinder");
+let convertedAxisOutput = document.getElementById("converted-axis");
+
+//sphere - cylinder. Axis + or minus 90
+let calculateSphereConversion = () => {
+    let convertedSphere = inputtedSphere.value - inputtedCylinder.value;
+
+    if (convertedSphere > 0) {
+        convertedSphereOutput.innerText = "+" + convertedSphere.toFixed(2);
+    }
+    else {
+        convertedSphereOutput.innerText = convertedSphere.toFixed(2);
+    }
+}
+
+let calculateAxisConversion = () => {
+    if (inputtedAxis.value <= 90) {
+        convertedAxisOutput.innerText = "@ " + ( Number(inputtedAxis.value) + 90 ) + "\u00B0";
+    }
+    else 
+    {
+        convertedAxisOutput.innerText = "@ " + ( Number(inputtedAxis.value) - 90 ) + "\u00B0";
+    }
+}
+
+let calculateCylinderConversion = () => {
+    if (inputtedCylinder.value > 0) {
+        convertedCylinderOutput.innerText = "+" + Number(inputtedCylinder.value).toFixed(2);
+    }
+    else 
+    {
+        convertedCylinderOutput.innerText = Number(inputtedCylinder.value).toFixed(2);
+    }
+}
 
 let convert = () => {
-    convertedSphere.innerText = inputtedSphere.value;
-    convertedCylinder.innerText = inputtedCylinder.value;
-    convertedAxis.innerText = inputtedAxis.value;
+    calculateSphereConversion();
+    calculateAxisConversion();
+    calculateCylinderConversion()
+    //cinputtedCylinder.value;
+    //inputtedAxis.value;
 }
 
 convertButton.addEventListener('click', () => {
